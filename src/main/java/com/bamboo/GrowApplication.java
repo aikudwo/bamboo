cd626f756ddb89dc7319dcfe8077fe84fb609f5c
wanglushun <aikudwo@outlook.com>
2020-05-19 10:04:04 +0800

    小的测试

--- src/main/java/com/bamboo/GrowApplication.java
+++ src/main/java/com/bamboo/GrowApplication.java
@@ -12,7 +12,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableCaching
 //@MapperScan("com.bamboo.mapper")
 public class GrowApplication {
-
+	//？？？？？？
 	public static void main(String[] args) {
 		SpringApplication.run(GrowApplication.class, args);
 
